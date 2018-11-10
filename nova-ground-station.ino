3d155f114c8fec9613cc422214711a9c1bcb865d
Emilio Mendoza <emilio2601@gmail.com>
2018-11-10 11:58:41 -0600

    Working tree sync

--- nova-ground-station.ino
+++ nova-ground-station.ino
@@ -7,31 +7,69 @@
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-void debug(String str){
-  /*
-   * Prints a given message to serial in a friendly debug format, like this:
-   * [1.334] Radio: TX power increased to 23 dBm
-   * 
-   * @param str Message to print to Serial output
-   */
+uint8_t buf[35] = {};
+uint8_t len     = sizeof(buf);
+
+const char* START_MODULE   = "Starting module";
+const char* SUCCESS_MODULE = "Module initialized";
+const char* FAILURE_MODULE = "Could not initialize module";
+
+void debug(const char* str){
   Serial.print("[");
   Serial.print(millis()/1000.0, 3);
   Serial.print("] ");
   Serial.println(str);
 }
 
-void initRadioSubsystem(){
-  debug("Radio: Starting subsystem");
+void moduleStatusMessage(const char* name, const char* msg){
+  Serial.print("[");
+  Serial.print(millis()/1000.0, 3);
+  Serial.print("] ");
+  Serial.print(name);
+  Serial.print(": ");
+  Serial.println(msg);
+}
+
+bool customRecv(uint8_t* buf, uint8_t* len) {
+  if(true || rf95.available() && buf && len){
+    ATOMIC_BLOCK_START;
+      memcpy(buf, rf95._buf, *len);
+    ATOMIC_BLOCK_END;
+    rf95.clearRxBuf();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+void initRadioSubsystem() {
+  moduleStatusMessage("Radio", START_MODULE);
  
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
   if(!rf95.init()) {
-    debug("Radio: Subsystem init failed - halting");
+    debug("Radio: module init failed - halting");
     while(1) {;}
   }
 
-  debug("Radio: Module initialized");
+  if(!rf95.setFrequency(915.00)) {
+    debug("Radio:   Could not set frequency to 915.00 MHz");
+    while(1) {;}
+  } else {
+    debug("Radio:   Frequency: 915.00 MHz");
+  }
+
+  rf95.setModeRx();
+  rf95.spiWrite(0x1d, 0x39);  //BW: 125 kHz, CR: 4/8, implicit header
+  //rf95.spiWrite(0x1e, 0xa4);  //SF: 10, no CRC
+  //rf95.spiWrite(0x26, 0x0c);  //Mobile node, AGC on
+  rf95.setPreambleLength(6);  //Awful waste of air time, but whatever...
+  debug("Radio:   BW: 125 kHz, CR: 4/8, SF: 10,  AGC: on, CRC: off");
+  debug("Radio:   Preamble length: 6 symbols");
+
+  moduleStatusMessage("Radio", SUCCESS_MODULE);
 }
 
 void setup() {
@@ -41,5 +79,10 @@ void setup() {
 }
 
 void loop() {
-
+  if(rf95.recv(buf, &len)){
+    uint32_t packet_ctr = buf[0] | (buf[1] << 8);
+    Serial.println(packet_ctr);
+  } else {
+    //Serial.println("Nothing");
+  }
 }
