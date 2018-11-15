b96a4fe587c779c270821189b38b726e91475766
Emilio Mendoza <emilio2601@gmail.com>
2018-11-14 23:05:25 -0600

    Ya quedó

--- nova-ground-station.ino
+++ nova-ground-station.ino
@@ -9,8 +9,8 @@
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-uint8_t buf[35]        = {};
-uint8_t bufHex[71]     = "Hello, world!";
+uint8_t buf[20]        = {};
+uint8_t bufHex[100]     = "";
 uint8_t len            = sizeof(buf);
 
 const size_t bufferSize = JSON_OBJECT_SIZE(18);
@@ -42,12 +42,13 @@ void moduleStatusMessage(const char* name, const char* msg){
 }
 
 int bufToHex(uint8_t *src, uint8_t len, uint8_t *dst){
+  memset(dst, 0, 100);
   const char* alphabet = "0123456789ABCDEF";
   for(size_t i = 0; i < len; i++) {
-    dst[i * 2]        = alphabet[(src[i] >> 4) & 0xF]; //Higher nibble first
-    dst[(i * 2) + 1]  = alphabet[src[i] & 0xF];
+    char snum[5];
+    itoa(src[i], snum, 16);
+    strcat(dst, snum);
   }
-  dst[len*2] = 0; //Null terminator
 }
 
 void initRadioSubsystem() {
@@ -68,12 +69,12 @@ void initRadioSubsystem() {
     debug("Radio:   Frequency: 915.00 MHz");
   }
   
-  rf95.setModemConfig(2);
+  //rf95.setModemConfig(2);
   //rf95.spiWrite(0x1d, 0x78);  //BW: 62.5 kHz, CR: 4/8, explicit header
   //rf95.spiWrite(0x1e, 0x94);  //SF: 9
   //rf95.spiWrite(0x26, 0x0c);  //TBD
 
-  rf95.setPreambleLength(6);  //Awful waste of air time, but whatever...
+  //rf95.setPreambleLength(6);  //Awful waste of air time, but whatever...
   rf95.setModeRx();
 
   debug("Radio:   BW: 62.5 kHz, CR: 4/8, SF: 10, AGC: on, CRC: off");
@@ -92,38 +93,22 @@ void loop() {
   if(rf95.recv(buf, &len)){
     uint32_t packetCtr   = buf[0]  | ((uint32_t) buf[1]  << 8);
     uint32_t timeBoot    = buf[2]  | ((uint32_t) buf[3]  << 8) | ((uint32_t) buf[4]  << 16);
-    uint16_t temperature = buf[5]  | ((uint32_t) buf[6]  << 8);
-    uint32_t pressure    = buf[7]  | ((uint32_t) buf[8]  << 8) | ((uint32_t) buf[9]  << 16);
-    int16_t  accX        = buf[10] | ((uint32_t) buf[11] << 8);
-    int16_t  accY        = buf[12] | ((uint32_t) buf[13] << 8);
-    int16_t  accZ        = buf[14] | ((uint32_t) buf[15] << 8);
-    int32_t  latitude    = buf[16] | ((uint32_t) buf[17] << 8) | ((uint32_t) buf[18] << 16) | ((uint32_t) buf[19] << 24);
-    int32_t  longitude   = buf[20] | ((uint32_t) buf[21] << 8) | ((uint32_t) buf[22] << 16) | ((uint32_t) buf[23] << 24);
-    int32_t  altitude    = buf[24] | ((uint32_t) buf[25] << 8) | ((uint32_t) buf[26] << 16);
-    uint32_t speed       = buf[27] | ((uint32_t) buf[28] << 8) | ((uint32_t) buf[29] << 16);
-    int16_t  magX        = 0;
-    int16_t  magY        = 0;
-    int16_t  magZ        = 0;
-    uint8_t  satellites  = (buf[34] >> 4) & 0xF;
-    bool     isValidLoc  = buf[34] >> 7;
+    int32_t  latitude    = buf[5]  | ((uint32_t) buf[6] << 8) | ((uint32_t) buf[7] << 16) | ((uint32_t) buf[8] << 24);
+    int32_t  longitude   = buf[9]  | ((uint32_t) buf[10] << 8) | ((uint32_t) buf[11] << 16) | ((uint32_t) buf[12] << 24);
+    int32_t  altitude    = buf[13] | ((uint32_t) buf[14] << 8) | ((uint32_t) buf[15] << 16);
+    uint32_t speed       = buf[16] | ((uint32_t) buf[17] << 8) | ((uint32_t) buf[18] << 16);
+    uint8_t  satellites  = (buf[19] >> 4) & 0xF;
+    bool     isValidLoc  = buf[19] >> 7;
     int16_t  lastSNR     = -133 + rf95.spiRead(0x1b);
 
     bufToHex(buf, len, bufHex);
 
     root["packetCtr"]   = packetCtr;
     root["timeBoot"]    = timeBoot;
-    root["temperature"] = temperature;
-    root["pressure"]    = pressure;
-    root["accX"]        = accX;
-    root["accY"]        = accY;
-    root["accZ"]        = accZ;
     root["latitude"]    = latitude;
     root["longitude"]   = longitude;
     root["altitude"]    = altitude;
     root["speed"]       = speed;
-    root["magX"]        = magX;
-    root["magY"]        = magY;
-    root["magZ"]        = magZ;
     root["satellites"]  = satellites;
     root["isValidLoc"]  = isValidLoc;
     root["lastSNR"]     = lastSNR;
